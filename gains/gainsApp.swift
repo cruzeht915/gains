300bbcd1ee80d560420d51046aa2a2722714128d
Julio Rodriguez <juliorod@mit.edu>
2025-01-06 18:38:32 -0500

    Initial Commit

---
+++ gains/gainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  gainsApp.swift
+//  gains
+//
+//  Created by Julio Rodriguez on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct gainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
