6e228c84069d31be9dee43f5536299d2606202fd
Elmer Cruz'git config --global user.name Elmer <elmercruzgalvan2019@gmail.com>
2025-02-02 21:27:44 -0600

    Implemented simple visual for Authorisation page

---
+++ gains/AuthView.swift
@@ -0,0 +1,114 @@
+//
+//  AuthView.swift
+//  gains
+//
+//  Created by Elmer Cruz on 1/31/25.
+//
+
+import SwiftUI
+struct AuthView: View{
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var confirmedPassword: String = ""
+    
+    @State var showSignUp: Bool = true
+    @State var sucessfulLogin: Bool = false
+    @State var sucessfulSignUp: Bool = false
+    
+    var body: some View {
+        ZStack (alignment: .top) {
+            Color("Background1")   // Background color
+                .ignoresSafeArea()
+            
+            VStack(spacing: 50){
+                Image("Sign Up or Log In")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 40)
+                    .padding(40)
+                
+                
+                
+                VStack (spacing: 15){
+                    HStack {
+                        Button (action: {
+                            showSignUp = false
+                            sucessfulLogin = true}
+                        ) {Image("Button (Log In)")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 60)
+                            
+                        }
+                        .navigationDestination(isPresented: $sucessfulLogin) {
+                            QuestionnaireView()
+                        }
+                        
+                        Button (action: {
+                            showSignUp = true
+                            sucessfulSignUp = true
+                        }
+                        ) {Image("Button (Sign Up)")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 60)
+                        }.navigationDestination(isPresented: $sucessfulSignUp) {
+                            HomeView()
+                        }
+                        
+                    }
+                    
+                    
+                    ZStack{
+                        Image("Email")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 70)
+                        TextField("Email", text: $email)
+                            .foregroundColor(.black)
+                            .frame(width: 250, height: 50)
+                            .disableAutocorrection(true)
+                            .offset(x: 0, y: 15)
+                    }
+                    
+                    ZStack {
+                        Image("Password")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 70)
+                        TextField("Password", text: $password)
+                            .foregroundColor(.black)
+                            .frame(width: 250, height: 50)
+                            .disableAutocorrection(true)
+                            .offset(x: 0, y: 15)
+                    }
+                    
+                    if showSignUp {
+                        ZStack {
+                            Image("Confirm Password")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 70)
+                            TextField("Password", text: $confirmedPassword)
+                                .foregroundColor(.black)
+                                .frame(width: 250, height: 50)
+                                .disableAutocorrection(true)
+                                .offset(x: 0, y: 15)
+                        }
+                    }
+                    
+                    
+                }
+                
+                VStack {
+                    Image("Button (Continue with Apple)")
+                    Image("Button (Continue with Google)")
+                }
+            }.padding()
+        }
+    }
+}
+
+#Preview {
+    AuthView()
+}
---
+++ gains/QuestionnaireView.swift
@@ -0,0 +1,23 @@
+//
+//  QuestionnaireView.swift
+//  gains
+//
+//  Created by Elmer Cruz on 2/2/25.
+//
+
+import SwiftUI
+
+struct QuestionnaireView: View {
+    var body: some View {
+        ZStack{
+            Color("Background1")   // Background color
+                .ignoresSafeArea()
+            
+            VStack(spacing: 50){}
+        }
+    }
+}
+
+#Preview {
+    QuestionnaireView()
+}
