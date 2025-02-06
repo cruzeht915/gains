653a86c87797733e894507b7ff15bd1c501e8049
Elmer Cruz'git config --global user.name Elmer <elmercruzgalvan2019@gmail.com>
2025-02-06 15:39:40 -0600

    Heatmap now includes a key to identify colors

--- gains/HeatmapView.swift
+++ gains/HeatmapView.swift
@@ -11,6 +11,8 @@ public struct HeatmapView: View {
     @State private var personalMuscleUse: [String: Int] = ["chest":3, "frontDelts":4 , "bis":4, "abs":2, "obliques":2, "quads":1, "adductors":1, "forearms":4, "traps":0, "lats":0, "rhombs":0, "erectors":0, "tris":3, "calves":2, "hams":1, "glutes":1, "abductors":1, "rearDelts":0]
     private var frontMuscles: [String] = ["chest", "frontDelts", "bis", "abs", "obliques", "quads", "adductors", "forearms"]
     private var rearMuscles: [String] = ["traps", "lats", "rhombs", "erectors", "tris", "calves", "hams", "glutes", "abductors", "rearDelts"]
+    private var muscleWork: [String] = ["Underworked", "Moderately Worked", "Balanced", "Overworked"];
+    
     public var body: some View {
         ZStack(alignment: .top) {
             Color("Background3")
@@ -26,7 +28,7 @@ public struct HeatmapView: View {
                     }
                 }.padding()
                 Image("Heat map")
-                    .padding(40)
+                    .padding(20)
                 HStack {
                     ZStack {
                         Image("frontMuscles")
@@ -57,8 +59,24 @@ public struct HeatmapView: View {
                         }
                     }
                 }
+                
+                HStack{
+                    ForEach(1...4, id: \.self){index in
+                        Rectangle()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(Color("MColor\(index)"))
+                                .cornerRadius(4)
+                        Text("\(muscleWork[index-1])")
+                            .foregroundColor(.white)
+                            .font(.caption)
+                        
+                        
+                    }
+                }.padding(.vertical)
+                
                 Text("Summmary:...")
                     .foregroundColor(.white)
+                    .padding()
             }
         }
     }
